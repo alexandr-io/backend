fdb3b62a1b1aab727b4baa40b6d4aa6e680334e3
Lucas <lucas.claisse@epitech.eu>
2020-10-17 19:21:00 +0200

    feat: Change application deployment process (script)

--- kubernetes/app-deployment.sh
+++ kubernetes/app-deployment.sh
@@ -8,13 +8,15 @@ if [ ! -d "$1" ]; then
     mkdir $1
     echo -e "\033[0;33mMoving in the folder...\033[0m"
     cd $1
+    echo -e "\033[0;33mCreating the k8s namespace $1...\033[0m"
+    kubectl create namespace $1
     echo -e "\033[0;33mChecking if preprod or prod deployment in progress...\033[0m"
-    if [ $2 = "preprod" ]; then
-        echo -e "\033[0;33mFind preprod deployment, downloading preprod $1 deployment file on the develop branch\033[0m"
-        curl -H "Authorization: token $3" -H "Accept: application/vnd.github.v3.raw" -O -L https://raw.githubusercontent.com/alexandr-io/backend/develop/microservices/$1/deployment/preprod/app/Deployment.yaml
+    if [ $(printenv HOST_TYPE) = "PREPROD" ]; then
+        echo -e "\033[0;33mFind preprod deployment, downloading preprod $1 deployment folder on the develop branch\033[0m"
+        echo "yes" | svn export https://github.com/alexandr-io/backend/branches/develop/microservices/$1/deployment/preprod --username=$(printenv SVN_USERNAME) --password=$(printenv SVN_PASSWORD)
     else
         echo -e "\033[0;33mFind prod deployment, downloading preprod $1 deployment file on the master branch\033[0m"
-        curl -H "Authorization: token $3" -H "Accept: application/vnd.github.v3.raw" -O -L https://raw.githubusercontent.com/alexandr-io/backend/master/microservices/$1/deployment/prod/app/Deployment.yaml
+        echo "yes" | svn export https://github.com/alexandr-io/backend/trunk/microservices/$1/deployment/prod --username=$(printenv SVN_USERNAME) --password=$(printenv SVN_PASSWORD)
     fi
     echo -e "\033[0;33mDownload done\033[0m"
 else
@@ -22,7 +24,7 @@ else
     cd $1
     echo -e "\033[0;33mDeleting old application...\033[0m"
     kubectl delete -f Deployment.yaml
+    echo -e "\033[0;33mCreating new application...\033[0m"
+    kubectl apply -f Deployment.yaml
+    echo -e "\033[0;33mApplication running.\033[0m"
 fi
-echo -e "\033[0;33mCreating new application...\033[0m"
-kubectl apply -f Deployment.yaml
-echo -e "\033[0;33mApplication running.\033[0m"
