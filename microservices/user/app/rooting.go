4ba9a2e99c9674b5e3273cb7820a237970696268
victorneuret <victor.neuret@gmail.com>
2020-10-22 10:46:29 +0900

    feat(user): Add log middleware
    Add recover middleware
    Add dashboard
    Add custom 404 handler
    Add doc to production Dockerfile for Auth MS

--- microservices/user/app/rooting.go
+++ microservices/user/app/rooting.go
@@ -5,17 +5,39 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/valyala/fasthttp/fasthttpadaptor"
 )
 
 // createRoute creates all the routes of the service.
 func createRoute(app *fiber.App) {
-
+	// Recover middleware in case of panic
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		TimeFormat: "2 Jan 15:04:05 MST",
+		TimeZone:   "Europe/Paris",
+		Next: func(ctx *fiber.Ctx) bool {
+			if string(ctx.Request().RequestURI()) == "/dashboard" ||
+				string(ctx.Request().RequestURI()) == "/docs" ||
+				string(ctx.Request().RequestURI()) == "/swagger.yml" {
+				return true
+			}
+			return false
+		},
+	}))
+	app.Get("/dashboard", monitor.New())
 	app.Get("/docs", wrapDocHandler())
 	app.Get("/swagger.yml", wrapFileServer())
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
 		return c.SendString("pong")
+	})
+
+	// Custom 404 handler
+	app.Use(func(c *fiber.Ctx) error {
+		return fiber.ErrNotFound
 	})
 }
 
