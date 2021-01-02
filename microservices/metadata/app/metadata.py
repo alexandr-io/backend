3e13a83ff01f55c62728d12a91978cbbcfdbbe04
Louis ANELLI <louisanelli@protonmail.ch>
2021-01-02 16:44:44 +0000

    Fixed print messages to work in Docker logs

--- microservices/metadata/app/metadata.py
+++ microservices/metadata/app/metadata.py
@@ -13,22 +13,23 @@ def get_metadata(query: str, author: str = ""):
     metadata["title"] = response['items'][0]["volumeInfo"]["title"]
     metadata["author"] = response['items'][0]["volumeInfo"]["authors"]
     metadata["author"] = ', '.join(metadata["author"])
-    metadata["publisher"] = response['items'][0]["volumeInfo"]["publisher"]
-    metadata["publishedDate"] = response['items'][0]["volumeInfo"]["publishedDate"]
-    metadata["description"] = response['items'][0]["volumeInfo"]["description"]
+    #metadata["publisher"] = response['items'][0]["volumeInfo"]["publisher"]
+    #metadata["publishedDate"] = response['items'][0]["volumeInfo"]["publishedDate"]
+    #metadata["description"] = response['items'][0]["volumeInfo"]["description"]
     return metadata
 
 
+print("STARTING", flush=True)
 bootstrap_servers=os.environ['KAFKA_URL']
 api_key = os.environ['GOOGLE_BOOKS_API_KEY']
 consumer = KafkaConsumer('metadata.retrieve', group_id='metadata.retrieve', bootstrap_servers=bootstrap_servers)
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
-print("STARTING")
+print("STARTING2", flush=True)
 
 for msg in consumer:
-    print("MESSAGE RECEIVED")
+    print("MESSAGE RECEIVED", flush=True)
     data = json.loads(msg.value)
     title = data["title"]
     metadata = get_metadata(title)
-    print(metadata)
+    print(metadata, flush=True)
     producer.send(topic='metadata.retrieve.response', value=bytes(json.dumps(metadata), "utf-8"), key=msg.key)
