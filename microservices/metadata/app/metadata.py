6b299a01c8e9818ee96c0b4bf17b3cfcfde33dbc
Louis ANELLI <louisanelli@protonmail.ch>
2021-01-04 14:42:45 +0000

    feat: Improved type hinting and readability

--- microservices/metadata/app/metadata.py
+++ microservices/metadata/app/metadata.py
@@ -1,20 +1,20 @@
 import json
 import os
 import requests
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 from kafka import KafkaConsumer, KafkaProducer
 
 
-def get_book_metadata(query: str, author: str = "") -> Dict[str, str]:
-    items: [str] = ["title", "authors", "publisher", "publishedDate", "pageCount", "categories", "maturityRating",
-                    "language",
-                    "imageLinks", "description"]
+def get_book_metadata(api_key: str, query: str, author: str = None) -> Dict[str, str]:
+    items: List[str] = ["title", "authors", "publisher", "publishedDate", "pageCount", "categories", "maturityRating",
+                        "language",
+                        "imageLinks", "description"]
     metadata: Dict[str, str] = {}
-    author: str = "+inauthor:" + author if author else author
+    author: str = "+inauthor:" + author if author else ""
 
     try:
-        response: Dict[Any] = requests.get(
+        response: Dict[str, Any] = requests.get(
             "https://www.googleapis.com/books/v1/volumes?q=" + query + author + "&key=" + api_key).json()
     except Exception as e:
         raise ConnectionError("Could not contact Google Books API :", e)
@@ -25,26 +25,30 @@ def get_book_metadata(query: str, author: str = "") -> Dict[str, str]:
     return metadata
 
 
-print("STARTING", flush=True)
-bootstrap_servers: str = os.environ['KAFKA_URL']
-api_key: str = os.environ['GOOGLE_BOOKS_API_KEY']
-consumer: KafkaConsumer = KafkaConsumer('metadata.retrieve', group_id='metadata.retrieve',
-                                        bootstrap_servers=bootstrap_servers)
-producer: KafkaProducer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+def main():
+    print("STARTING", flush=True)
+    bootstrap_servers: str = os.environ['KAFKA_URL']
+    api_key: str = os.environ['GOOGLE_BOOKS_API_KEY']
+    consumer: KafkaConsumer = KafkaConsumer('metadata.retrieve', group_id='metadata.retrieve',
+                                            bootstrap_servers=bootstrap_servers)
+    producer: KafkaProducer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
-for msg in consumer:
-    print("MESSAGE RECEIVED :", msg.value, flush=True)
-    data: Dict[str, str] = json.loads(msg.value)
-    metadata: Dict[str, str] = {}
+    for msg in consumer:
+        print("MESSAGE RECEIVED :", msg.value, flush=True)
+        data: Dict[str, str] = json.loads(msg.value)
 
-    if not data or not data["title"]:
-        raise ValueError("The title of the book is required")
+        if not data or not data["title"]:
+            raise ValueError("The title of the book is required")
 
-    title = data["title"]
-    if "authors" in data:
-        metadata = get_book_metadata(title, data["authors"])
-    else:
-        metadata = get_book_metadata(title)
+        title = data["title"]
+        if "authors" in data:
+            metadata: Dict[str, str] = get_book_metadata(api_key, title, data["authors"])
+        else:
+            metadata: Dict[str, str] = get_book_metadata(api_key, title)
 
-    print(metadata, flush=True)
-    producer.send(topic='metadata.retrieve.response', value=bytes(json.dumps(metadata), "utf-8"), key=msg.key)
+        print(metadata, flush=True)
+        producer.send(topic='metadata.retrieve.response', value=bytes(json.dumps(metadata), "utf-8"), key=msg.key)
+
+
+if __name__ == '__main__':
+    main()
