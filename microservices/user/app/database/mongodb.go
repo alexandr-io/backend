09f69b5e2bc592a1555a637370e9bdf8cbc57be0
Lucas <lucas.claisse@epitech.eu>
2021-01-15 18:34:33 +0900

    feat: Use 1 URI for mongo connection in mongodb.go

--- microservices/user/app/database/mongodb.go
+++ microservices/user/app/database/mongodb.go
@@ -24,24 +24,15 @@ var Instance InstanceData
 // Database settings
 const dbName = "user"
 
-var mongoURI = fmt.Sprintf(
-	"mongodb+srv://%s:%s@%s/%s?authSource=admin&readPreference=primary&appname=UserService&ssl=false",
-	os.Getenv("MONGO_INITDB_ROOT_USERNAME"),
-	os.Getenv("MONGO_INITDB_ROOT_PASSWORD"),
-	os.Getenv("MONGO_URL"),
-	dbName)
-
 // ConnectToMongo is connecting the service to mongodb using mongoURI.
 // After success, instanceMongo is filled with the db client and db handler.
 func ConnectToMongo() {
-	if _, ok := os.LookupEnv("DEV"); ok {
-		mongoURI = fmt.Sprintf(
-			"mongodb://%s:%s@%s:27017/%s?authSource=admin&readPreference=primary&appname=UserService&ssl=false",
-			os.Getenv("MONGO_INITDB_ROOT_USERNAME"),
-			os.Getenv("MONGO_INITDB_ROOT_PASSWORD"),
-			os.Getenv("MONGO_URL"),
-			dbName)
-	}
+	mongoURI := fmt.Sprintf(
+		"mongodb://%s:%s@%s:27017/%s?authSource=admin&readPreference=primary&appname=UserService&ssl=false",
+		os.Getenv("MONGO_INITDB_ROOT_USERNAME"),
+		os.Getenv("MONGO_INITDB_ROOT_PASSWORD"),
+		os.Getenv("MONGO_URL"),
+		dbName)
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	if err != nil {
