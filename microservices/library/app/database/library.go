6b026279df0735f3770119301a809b04aee01789
Maxime Houis <maxime.houis@epitech.eu>
2021-01-24 22:26:49 +0100

    fix(library): add forgotten change from previous commit (CollectionBookUserData)

--- microservices/library/app/database/library.go
+++ microservices/library/app/database/library.go
@@ -22,6 +22,9 @@ const CollectionLibraries = "libraries"
 // CollectionLibrary is the name of the library collection in mongodb
 const CollectionLibrary = "library"
 
+// CollectionBookUserData is the name of the user data collection in mongodb
+const CollectionBookUserData = "book_user_data"
+
 //
 // Getters
 //
