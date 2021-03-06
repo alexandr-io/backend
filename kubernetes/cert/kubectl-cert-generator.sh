9a359cc39af5e4c2788624baf051531c66d11b53
LucasClaisse <lucas.claisse@epitech.eu>
2021-03-06 20:50:09 +0900

    feat(ansible,kubernetes): Add cert auto creation and required resources (#96)
    Auto creation of k8s certs to have access to the cluster from a developer laptop

--- kubernetes/cert/kubectl-cert-generator.sh
+++ kubernetes/cert/kubectl-cert-generator.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 USER="$1"
-BASE64_CSR=$(base64 < tmp.yaml | tr -d '\n')
+BASE64_CSR=$(base64 < "$1".csr | tr -d '\n')
+
+CLUSTER_CERTIFICATE_AUTHORITY_DATA=$(kubectl config view -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' --raw)
+
+NODE_NAME=$(kubectl get node -o jsonpath='{.items[0].metadata.name}')
 
 export USER
 export BASE64_CSR
+export NODE_NAME
 
-envsubst < tmp.yaml | kubectl apply -f -
+export CLUSTER_CERTIFICATE_AUTHORITY_DATA
+
+envsubst < csr.yaml | kubectl apply -f -
 
 kubectl certificate approve "$1"
 CLIENT_CERTIFICATE_DATA=$(kubectl get csr "$1" -o jsonpath='{.status.certificate}')
