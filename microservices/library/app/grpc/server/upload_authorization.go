1810f4951a986bab277063a54692380900ca3ffe
victorneuret <victor.neuret@gmail.com>
2021-04-14 12:01:45 +0900

    doc(media): Add doc for cover upload and download

--- microservices/library/app/grpc/server/upload_authorization.go
+++ microservices/library/app/grpc/server/upload_authorization.go
@@ -6,23 +6,14 @@ import (
 
 	"github.com/alexandr-io/backend/grpc"
 	grpclibrary "github.com/alexandr-io/backend/grpc/library"
-	"github.com/alexandr-io/backend/library/data"
 	"github.com/alexandr-io/backend/library/internal"
-
-	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // UploadAuthorization is a gRPC server method that check if a user can upload a book to a library
 func (s *server) UploadAuthorization(_ context.Context, in *grpclibrary.UploadAuthorizationRequest) (*grpclibrary.UploadAuthorizationReply, error) {
 	fmt.Printf("[gRPC]: Upload authorization received: %+v\n", in.String())
 
-	libraryID, err := primitive.ObjectIDFromHex(in.GetLibraryID())
-	if err != nil {
-		return nil, grpc.FiberErrorToGRPC(data.NewHTTPErrorInfo(fiber.StatusUnauthorized, err.Error()))
-	}
-
-	ok, err := internal.CanUserUploadOnLibrary(in.GetUserID(), libraryID.Hex())
+	ok, err := internal.CanUserUploadOnLibrary(in.GetUserID(), in.GetLibraryID())
 	if err != nil {
 		return nil, grpc.FiberErrorToGRPC(err)
 	}
