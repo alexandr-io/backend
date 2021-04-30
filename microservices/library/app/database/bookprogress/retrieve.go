e94454521a41a71d15fd65acca35a1d0db1740f0
Maxime Houis <maxime.houis@epitech.eu>
2021-04-30 21:17:01 +0200

    style(library): fix import

--- microservices/library/app/database/bookprogress/retrieve.go
+++ microservices/library/app/database/bookprogress/retrieve.go
@@ -2,7 +2,6 @@ package bookprogress
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 
 	"github.com/alexandr-io/backend/library/data"
@@ -10,6 +9,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
