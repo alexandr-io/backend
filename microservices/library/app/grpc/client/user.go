a709eaa51c65e0fa94568ce7ecb34893287f34de
Lucas <lucas.claisse@epitech.eu>
2021-05-18 19:25:42 +0900

    feat(library): add user invitation route
    - new HTTP route to invite a user to a library
    - new 'has_access' & 'is_invited' fields in Libraries getter route

---
+++ microservices/library/app/grpc/client/user.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/alexandr-io/backend/grpc"
+	grpcuser "github.com/alexandr-io/backend/grpc/user"
+	"github.com/alexandr-io/backend/library/data"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func UserFromLogin(ctx context.Context, login string) (*data.User, error) {
+	if userClient == nil {
+		return nil, data.NewHTTPErrorInfo(fiber.StatusInternalServerError, "gRPC user client not initialized")
+	}
+
+	userRequest := grpcuser.UserFromLoginRequest{Login: login}
+	fmt.Printf("[gRPC]: UserFromLogin send: %+v\n", userRequest.String())
+	userReply, err := userClient.UserFromLogin(ctx, &userRequest)
+	if err != nil {
+		return nil, grpc.ErrorToFiber(err)
+	}
+
+	return &data.User{
+		ID:       userReply.ID,
+		Username: userReply.Username,
+		Email:    userReply.Email,
+	}, nil
+}
---
+++ microservices/library/app/handlers/library_invite.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"github.com/alexandr-io/backend/library/data"
+	"github.com/alexandr-io/backend/library/database/libraries"
+	"github.com/alexandr-io/backend/library/grpc/client"
+	"github.com/alexandr-io/backend/library/internal"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func LibraryInvite(ctx *fiber.Ctx) error {
+	ctx.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	userID, err := userIDFromHeader(ctx)
+	if err != nil {
+		return err
+	}
+	libraryID, err := getLibraryIDFromParams(ctx)
+	if err != nil {
+		return err
+	}
+	if perm, err := internal.GetUserLibraryPermission(userID.Hex(), libraryID.Hex()); err != nil {
+		return err
+	} else if perm.CanInviteUser() == false {
+		return data.NewHTTPErrorInfo(fiber.StatusUnauthorized, "You are not allowed to invite user in this library")
+	}
+
+	var libraryInvite data.LibraryInvite
+	if err := ParseBodyJSON(ctx, &libraryInvite); err != nil {
+		return err
+	}
+
+	user, err := client.UserFromLogin(ctx.Context(), libraryInvite.Login)
+	if err != nil {
+		return err
+	}
+
+	InvitedUserID, err := primitive.ObjectIDFromHex(user.ID)
+	if err != nil {
+		return data.NewHTTPErrorInfo(fiber.StatusInternalServerError, err.Error())
+	}
+
+	userLibrary, err := libraries.Insert(data.UserLibrary{
+		UserID:      InvitedUserID,
+		LibraryID:   libraryID,
+		Permissions: libraryInvite.Permissions,
+		Groups:      libraryInvite.Groups,
+		InvitedBy:   userID,
+	})
+	if err != nil {
+		return err
+	}
+
+	if err := ctx.Status(fiber.StatusCreated).JSON(userLibrary); err != nil {
+		return data.NewHTTPErrorInfo(fiber.StatusInternalServerError, err.Error())
+	}
+	return nil
+}
