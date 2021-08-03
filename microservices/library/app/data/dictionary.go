c54ac7b6240b0ac1f8ba161a15b9574036843d90
Louis ANELLI <louis.anelli@epitech.eu>
2021-08-03 16:36:29 +0100

    feat: Added dictionary word retrieval

---
+++ microservices/library/app/data/dictionary.go
@@ -0,0 +1,23 @@
+package data
+
+type Phonetics struct {
+	Text  string `json:"text"`
+	Audio string `json:"audio"`
+}
+
+type Definitions struct {
+	Definition string   `json:"definition"`
+	Synonyms   []string `json:"synonyms"`
+	Example    string   `json:"example"`
+}
+
+type Meanings struct {
+	PartOfSpeech string         `json:"partOfSpeech"`
+	Definitions  []*Definitions `json:"definitions"`
+}
+
+type DictResponse struct {
+	Word      string       `json:"word"`
+	Phonetics []*Phonetics `json:"phonetics"`
+	Meanings  []*Meanings  `json:"meanings"`
+}
---
+++ microservices/library/app/handlers/dict_retrieve.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/alexandr-io/backend/library/data"
+	"github.com/gofiber/fiber/v2"
+	"io/ioutil"
+	"net/http"
+)
+
+// DictionaryRetrieve gets the definition of a queried word with a queried language
+func DictionaryRetrieve(ctx *fiber.Ctx) error {
+	ctx.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	queriedWord := ctx.Params("queried_word")
+	queriedLang := ctx.Params("lang")
+
+	url := "https://api.dictionaryapi.dev/api/v2/entries/" + queriedLang + "/" + queriedWord
+
+	response, err := http.Get(url)
+	if err != nil {
+		return data.NewHTTPErrorInfo(fiber.StatusInternalServerError, err.Error())
+	}
+
+	definitionByes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return data.NewHTTPErrorInfo(fiber.StatusInternalServerError, err.Error())
+	}
+
+	var dictResponse []data.DictResponse
+
+	if response.Body != nil && response.StatusCode == 200 {
+		err := json.Unmarshal(definitionByes, &dictResponse)
+		if err != nil {
+			return data.NewHTTPErrorInfo(fiber.StatusInternalServerError, err.Error())
+		}
+	}
+
+	if err = ctx.Status(fiber.StatusOK).JSON(dictResponse); err != nil {
+		return data.NewHTTPErrorInfo(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return nil
+}
