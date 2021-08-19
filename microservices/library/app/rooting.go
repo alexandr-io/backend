3ee2679a0af9b441e4cd8c80cdb353e6e9708ea6
victorneuret <victor.neuret@gmail.com>
2021-08-19 06:46:54 +0200

    Add dictionary route after rebase

--- microservices/library/app/rooting.go
+++ microservices/library/app/rooting.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/alexandr-io/backend/library/handlers"
+	userMiddleware "github.com/alexandr-io/backend/library/middleware"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -37,6 +38,9 @@ func createRoute(app *fiber.App) {
 	handlers.CreateGroupHandlers(app)
 	handlers.CreatePermissionHandlers(app)
 	handlers.CreateMetadataHandlers(app)
+
+	// Retrieve definitions from dictionary API
+	app.Get("/dictionary/definition/:lang/:queried_word", userMiddleware.Protected(), handlers.DictionaryRetrieve)
 
 	// Custom 404 handler
 	app.Use(func(c *fiber.Ctx) error {
